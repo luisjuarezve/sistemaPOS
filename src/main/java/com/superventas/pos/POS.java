8b8a1d44d061364b34cc6b1e84088297662e1489
luisjuarezve <163745370+luisjuarezve@users.noreply.github.com>
2024-11-10 18:07:57 -0400

    fix bigs con formTasaDolar

--- src/main/java/com/superventas/pos/POS.java
+++ src/main/java/com/superventas/pos/POS.java
@@ -1,17 +1,6 @@
 package com.superventas.pos;
 
-import com.superventas.pos.model.Empleados;
-import com.superventas.pos.model.Inventario;
-import com.superventas.pos.model.Rol;
-import com.superventas.pos.persistence.EmpleadosDAO;
-import com.superventas.pos.persistence.InventarioDAO;
 import com.superventas.pos.view.Login;
-import com.superventas.pos.view.SuperPOS;
-import com.superventas.pos.view.components.CrearCliente;
-import com.superventas.pos.view.menus.PanelProductos;
-import java.awt.Dimension;
-
-
 
 public class POS {
 
